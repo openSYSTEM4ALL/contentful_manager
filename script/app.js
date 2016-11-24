dd641d57923cfaa90a00d5ebc271ca69f938fc01
vivek malik <malik_vivek@network.lilly.com>
2016-11-24 19:35:38 +0530

    iadding seperate sideNav for mobile sites

--- script/app.js
+++ script/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   $('#splashscreen').fadeOut(1000);
-  $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav({
+      menuWidth: 200, // Default is 240
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
   $('.parallax').parallax();
   $('ul li a').click(function () {
     $('li').removeClass("active");
