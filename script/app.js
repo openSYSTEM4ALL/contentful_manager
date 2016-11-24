1221939b82c7b99ec648286850715ed62855eba6
vivek malik <malik_vivek@network.lilly.com>
2016-11-24 20:22:25 +0530

    unsupported browser for iE page - adding

--- script/app.js
+++ script/app.js
@@ -11,3 +11,22 @@ $(document).ready(function () {
   });
 
 });
+
+
+
+function GetIEVersion() {
+  var sAgent = window.navigator.userAgent;
+  var Idx = sAgent.indexOf("MSIE");
+
+  // If IE, return version number.
+  if (Idx > 0) 
+    return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+
+  // If IE 11 then look for Updated user agent string.
+  else if (!!navigator.userAgent.match(/Trident\/7\./)) 
+    return 11;
+
+  else
+    return 0; //It is not IE
+}
+
