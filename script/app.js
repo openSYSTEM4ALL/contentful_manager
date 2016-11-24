9cef403bb129fa752a002f3ba3156f695fc5a3e0
vivek malik <malik_vivek@network.lilly.com>
2016-11-24 20:30:42 +0530

    fixing navbar

--- script/app.js
+++ script/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('#splashscreen').fadeOut(1000);
   $(".button-collapse").sideNav({
-      //menuWidth: 200, // Default is 240
+      menuWidth: 200, // Default is 240
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
   $('.parallax').parallax();
