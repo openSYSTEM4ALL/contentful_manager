bc84fe3976291b335624bd1a2cec2e012f3722fe
Ashutosh Sharma <sharma_ashutosh@network.lilly.com>
2016-11-25 14:39:00 +0530

    Asset field for other cms migration added

--- script/otherController.js
+++ script/otherController.js
@@ -9,16 +9,48 @@ app.controller('otherController', ['$scope', '$http', '$timeout', '$window', '$f
     });
     $('#ddlDestSpace').on('change', function (e) {
         if ($('#ddlDestSpace').siblings('.dropdown-content').find('li.active>span').text() != "") {
-            $scope.getDestAssets($('#ddlDestSpace').siblings('.dropdown-content').find('li.active>span').text());
+            $scope.getDestLocales($('#ddlDestSpace').siblings('.dropdown-content').find('li.active>span').text());
         }
     });
+    $('#ddlAssetLocale').on('change', function (e) {
+        if ($('#ddlAssetLocale').siblings('.dropdown-content').find('li.active>span').text() != "") {
+            $scope.setAssetLocale($('#ddlAssetLocale').siblings('.dropdown-content').find('li.active>span').text());
+        }
+    });
+    $scope.spaces = spac;
+    $scope.destLocales = [];
     $scope.assetList = [];
+
+    $scope.getDestLocales = function (destSpaceSelected) {
+
+        $scope.destitem = destSpaceSelected;
+        $scope.destitem = $filter('filter')($scope.spaces, {
+            space: destSpaceSelected
+        })[0];
+        $scope.destSpaceId = $scope.destitem.value;
+        $scope.destAccessToken = $scope.destitem.token;
+        $scope.destClient = contentfulManagement.createClient({
+            // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+            accessToken: $scope.destAccessToken
+        });
+
+        $scope.destClient.getSpace($scope.destSpaceId)
+            .then((space) => {
+                // Now that we have a space, we can get locales from that space
+                $scope.destSpace = space;
+                space.getLocales()
+                    .then((locales) => {
+                        $scope.destLocales = locales.items;
+                    })
+            });
+    }
 
     $scope.saveAssetToList = function () {
 
         if (btnAdd.value == "Update") {
             for (var a in $scope.assetList) {
                 if ($scope.assetList[a].assetName == $scope.assetName) {
+
                     $scope.assetList[a].assetTitle = $scope.assetTitle;
                     $scope.assetList[a].assetContentType = $scope.assetContentType;
                     $scope.assetList[a].assetUrl = $scope.assetUrl;
@@ -74,4 +106,10 @@ app.controller('otherController', ['$scope', '$http', '$timeout', '$window', '$f
         txtAssetName.readOnly = false;
         Materialize.toast('BOOM ! BOOM !', 4000);
     }
+
+    $scope.uploadToContentful = function () {
+
+    }
+
+
 }]);
