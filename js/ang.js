ee69de021afa56835c24dcbee4902fdbe758edb2
Ashutosh Sharma - Network <sharma_ashutosh@network.lilly.com>
2018-03-19 20:14:42 +0530

    Updating folder structure - removing angular as folder name

--- js/ang.js
+++ js/ang.js
@@ -3,39 +3,39 @@ var app = angular.module('myApp', ['ngRoute']);
 app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
     $routeProvider.when("/migrate-content-types", {
         controller: "contentTypeController",
-        templateUrl: "js/angular/templates/content-types-migration.html"
+        templateUrl: "js/templates/content-types-migration.html"
     });
     $routeProvider.when("/migrate-entries", {
         controller: "entriesController",
-        templateUrl: "js/angular/templates/entries-migration.html"
+        templateUrl: "js/templates/entries-migration.html"
     });
     $routeProvider.when("/migrate-entries-locale", {
         controller: "entriesLocaleController",
-        templateUrl: "js/angular/templates/entries-locale-migration.html"
+        templateUrl: "js/templates/entries-locale-migration.html"
     });
     $routeProvider.when("/migrate", {
         controller: "layoutController",
-        templateUrl: "js/angular/templates/migrate.html"
+        templateUrl: "js/templates/migrate.html"
     });
     $routeProvider.when("/login", {
         controller: "loginController",
-        templateUrl:"js/angular/templates/login.html"
+        templateUrl:"js/templates/login.html"
     });
     $routeProvider.when("/configure", {
         controller: "layoutController",
-        templateUrl: "js/angular/templates/configure.html"
+        templateUrl: "js/templates/configure.html"
     });
     $routeProvider.when("/upload", {
         controller: "uploadController",
-        templateUrl: "js/angular/templates/upload.html"
+        templateUrl: "js/templates/upload.html"
     });
     $routeProvider.when("/bulkupload", {
         controller: "bulkController",
-        templateUrl:"js/angular/templates/bulkupload.html"
+        templateUrl:"js/templates/bulkupload.html"
     });
     $routeProvider.when("/", {
         controller: "landingController",
-        templateUrl:"js/angular/templates/landing.html"
+        templateUrl:"js/templates/landing.html"
     });
 
     $routeProvider.otherwise({
